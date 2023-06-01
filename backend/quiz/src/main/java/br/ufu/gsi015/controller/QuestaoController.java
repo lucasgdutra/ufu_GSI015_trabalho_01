56f38bda5261832fb4267b770b20793e8ae6255d
dalmo123 <66819868+dalmo123@users.noreply.github.com>
2023-06-01 11:33:20 -0300

    Update QuestaoController.java

--- backend/quiz/src/main/java/br/ufu/gsi015/controller/QuestaoController.java
+++ backend/quiz/src/main/java/br/ufu/gsi015/controller/QuestaoController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -80,4 +81,11 @@ public class QuestaoController {
             throw new CustomInternalErrorException(e.getMessage());
         }
     }
+
+    @DeleteMapping("/{id0}")
+    public ResponseEntity<String> deleteQuestao(@Valid @PathVariable Long id){
+        return new ResponseEntity<String>(questaoService.deleteQuestao(id),HttpStatus.OK);
+    }
+
+
 }
